c7054ad48efd7e027002de9dc3f53f6884029636
mssbabou <mssbabou@gmail.com>
2021-10-05 19:34:11 +0200

    a

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 
-[RequireComponent(typeof(CapsuleCollider))]
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,10 +13,12 @@ public class PlayerMovement : MonoBehaviour
     [Space(10)]
     public float MoveSpeed = 6f;
     public float SprintSpeed = 12f;
+    public float MoveAcceleration = 0.1f;
     public float jumpForce = 10f;
     public int jumpAmount = 2;
-    
-    [Space(10)]
+
+    [Space(10)] 
+    public float slideFriction;
     public float turnSmoothTime = 0.1f;
     public float gravityMultiplier = 1f;
     public float groundDistance = 0.4f;
@@ -27,7 +28,11 @@ public class PlayerMovement : MonoBehaviour
     ThirdPersonCameraController PCC;
     float gravity = -9.81f;
     float turnSmoothVelocity;
+    float moveSmoothVelocity;
+    float targetAngle;
+    float anglef;
     bool isGrounded = true;
+    Vector3 hitNormal;
     Vector3 direction;
     Vector3 velocity;
     Vector2 look;
@@ -46,16 +51,17 @@ public class PlayerMovement : MonoBehaviour
         Cursor.visible = false;
     }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        hitNormal = hit.normal;
+        Debug.Log(hitNormal);
+    }
+
     void Update()
     {
-        // Change speed with sprint
-        speed = MoveSpeed;
-        if (UnityEngine.Input.GetButton("Sprint"))
-        {
-            speed = SprintSpeed;
-        }
         
-
+        
+        
         // Check if player is grounded
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if (isGrounded && velocity.y < 0)
@@ -70,17 +76,39 @@ public class PlayerMovement : MonoBehaviour
         direction.z = UnityEngine.Input.GetAxisRaw("Vertical");
         direction = direction.normalized;
         
+        // Change speed with sprint
+        if (UnityEngine.Input.GetButton("Sprint") && direction.magnitude >= 0.1f)
+        {
+            speed = Mathf.SmoothDamp(speed, SprintSpeed, ref moveSmoothVelocity, MoveAcceleration);
+        }
+        else if(direction.magnitude >= 0.1f)
+        {
+            speed = Mathf.SmoothDamp(speed, MoveSpeed, ref moveSmoothVelocity, MoveAcceleration);
+        }
+        else
+        {
+            speed = Mathf.SmoothDamp(speed, 0f, ref moveSmoothVelocity, MoveAcceleration);
+        }
+
+        if (speed < 0.2f)
+        {
+            speed = 0;
+        }
+        
+        
         if (direction.magnitude >= 0.1f)
         {
             // Rotate with camera
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + PCC.cam.transform.eulerAngles.y;
-            float anglef = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + PCC.cam.transform.eulerAngles.y;
+            anglef = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, anglef, 0f);
-                                
-            // Apply Rotation
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            
         }
+        
+        // Apply Rotation
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        Debug.Log(speed);
+        controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
         // Jump when "Jump" and "isGrounded" with jumpForce
         if (jumpsLeft >= 1 && UnityEngine.Input.GetButtonDown("Jump"))
@@ -107,6 +135,7 @@ public class PlayerMovement : MonoBehaviour
         {
             //Play falling animation
         }
+        
     }
     
     void OnDrawGizmos()
